11961f2d31d5f2c0c324b0cf46c6a3af29bd72ee
SuTart2 <151579488+SuTart2@users.noreply.github.com>
2024-04-07 11:25:28 -0700

    Update ProjectsList.js

--- src/pages/ProjectsList.js
+++ src/pages/ProjectsList.js
@@ -3,7 +3,7 @@ import "./styles.css"
 export default function ProjectsList() {
     return <div>
     <h1>Projects</h1>
-      <a href="../../Node JS/portfolio/mini project/index.html"> <button>Memory Game</button> </a>
+      <a href="https://ezmemorygame.netlify.app"> <button>Memory Game</button> </a>
       <a href="../../Node JS/portfolio/bootstrap project/painting1.html"> <button>Bootstrap paintings</button> </a>
       <a href=""><button>Weather App</button></a>
   </div>;
